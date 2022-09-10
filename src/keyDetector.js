5eaf8cb0761ee87585f029d9c212227c0cad34f8
Anthonii Veremko <anton.veremko@gmail.com>
2022-09-10 20:00:50 +0200

    fixed abuf on key detector where movement stops occured while different move contol keys were pressed

--- src/keyDetector.js
+++ src/keyDetector.js
@@ -1,32 +1,74 @@
-export default class keyDetector{
-    constructor(game, player){
-        document.addEventListener('keydown', (e)=>{
-            let key = e.key;
-            if (key === ' ') key = 'space';
-        
-            // console.log(key);
-            switch(key){
+export default class keyDetector {
+    constructor(game, player) {
+
+        function getPressedKeyName(e){
+            let keyName = e.key;
+            if (keyName === ' ') keyName = 'Space';
+            return keyName
+        }
+
+        function keyUpHandler(e) {
+            let key = getPressedKeyName(e);
+            switch (key) {
                 case 'ArrowLeft':
-                    player.direction.left = player.speed * -1
-                    player.direction.top = 0
+                    if(player.direction.left < 0) player.direction.left = 0;
                     break;
                 case 'ArrowRight':
-                    player.direction.left = player.speed
-                    player.direction.top = 0
+                    if(player.direction.left > 0) player.direction.left = 0;
                     break;
                 case 'ArrowUp':
-                    player.direction.top = player.speed * -1
-                    player.direction.left = 0
+                    if(player.direction.top < 0) player.direction.top = 0;
                     break;
                 case 'ArrowDown':
-                    player.direction.top = player.speed * 1
-                    player.direction.left = 0
+                    if(player.direction.top > 0) player.direction.top = 0;
                     break;
                 case 'Escape':
-                    if(game.isAnimationRunning) game.pause()
+                    if (game.isAnimationRunning) game.pause()
                     else game.resume()
                     break;
             }
+        }
+
+        let keyUpHandlerDelay;
+
+        document.addEventListener('keydown', (e) => {
+            let key = getPressedKeyName(e);
+
+            // console.log(key);
+            switch (key) {
+                case 'ArrowLeft':
+                    player.direction.left = player.speed * -1;
+                    player.direction.top = 0;
+                    clearTimeout(keyUpHandlerDelay);
+                    player.turn('left');
+                    break;
+                case 'ArrowRight':
+                    player.direction.left = player.speed;
+                    player.direction.top = 0;
+                    clearTimeout(keyUpHandlerDelay);
+                    player.turn('right');
+                    break;
+                case 'ArrowUp':
+                    player.direction.top = player.speed * -1;
+                    player.direction.left = 0;
+                    clearTimeout(keyUpHandlerDelay);
+                    player.turn('up');
+                    break;
+                case 'ArrowDown':
+                    player.direction.top = player.speed * 1;
+                    player.direction.left = 0;
+                    clearTimeout(keyUpHandlerDelay);
+                    player.turn('down');
+                    break;
+                case 'Escape':
+                    if (game.isAnimationRunning) game.pause();
+                    else game.resume();
+                    break;
+            }
+        })
+
+        document.addEventListener('keyup', (e)=>{
+            keyUpHandlerDelay = setTimeout(()=>keyUpHandler(e), 85)
         })
     }
 }
