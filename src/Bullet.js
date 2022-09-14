8e4ade49549fb6537a67f075eab0b08f1c031b45
Anthonii Veremko <anton.veremko@gmail.com>
2022-09-14 08:53:40 +0200

    Bullet update. Bullets will be draw on canvas and not as a div element on the page

--- src/Bullet.js
+++ src/Bullet.js
@@ -4,9 +4,6 @@ export default class Bullet{
 
         this.id = Date.now() * Math.floor(Math.random() * 100);
         this.player = player
-        let referenceElement = document.getElementById('bullet');
-        this.playerElem = referenceElement.cloneNode(true);
-        document.body.append(this.playerElem)
 
         this.flyRange = 200;
         this.startCoord = {
@@ -15,10 +12,10 @@ export default class Bullet{
         }
         this.flightDelta = 0;
 
-        this.moveVectorName = 'up';
+        this.moveVectorName = this.player.moveVectorName;
 
-        this.height = parseInt(getComputedStyle(this.playerElem).height);
-        this.width = parseInt(getComputedStyle(this.playerElem).width);
+        this.height = 10;
+        this.width = 10;
 
         this.speed = 10;
 
@@ -27,9 +24,6 @@ export default class Bullet{
 
         this.position = {}
         this.setPosition()
-
-        this.playerElem.style.top = this.position.top + 'px'
-        this.playerElem.style.left = this.position.left + 'px'
     }
 
     getDirectionName(){
@@ -41,22 +35,18 @@ export default class Bullet{
             case 'left':
                 this.direction.left = this.speed * -1;
                 this.direction.top = 0;
-                this.turn('left');
                 break;
             case 'right':
                 this.direction.left = this.speed;
                 this.direction.top = 0;
-                this.turn('right');
                 break;
             case 'up':
                 this.direction.top = this.speed * -1;
                 this.direction.left = 0;
-                this.turn('up');
                 break;
             case 'down':
                 this.direction.top = this.speed * 1;
                 this.direction.left = 0;
-                this.turn('down');
                 break;
         }
     }
@@ -87,33 +77,21 @@ export default class Bullet{
         }
     }
 
-    turn(direction){
-        switch(direction){
-            case 'left':
-                this.playerElem.style.transform = 'rotate(-90deg)';
-                break;
-            case 'right':
-                this.playerElem.style.transform = 'rotate(90deg)';
-                break;
-            case 'up':
-                this.playerElem.style.transform = 'rotate(0deg)';
-                break;
-            case 'down':
-                this.playerElem.style.transform = 'rotate(180deg)';
-                break;
-        }
-        this.moveVectorName = direction
-    }
-
     measureFlightDelta(){
         let delta = 0;
         if(this.moveVectorName === 'left' || this.moveVectorName === 'right' ){
-            delta = this.startCoord.left - parseInt(this.playerElem.style.left);
+            delta = this.startCoord.left - this.position.left;
         }else{
-            delta = this.startCoord.top - parseInt(this.playerElem.style.top);
+            delta = this.startCoord.top - this .position.top;
         }
         if(delta < 0) delta = delta * -1
         return delta
+    }
+
+    draw(){
+        let {screen} = this.player.game;
+        screen.fillStyle = 'black';
+        screen.fillRect(this.position.left, this.position.top, this.width, this.height)
     }
 
     move(){
@@ -122,25 +100,30 @@ export default class Bullet{
         let newBottomCoord = this.getCoords().bottom + this.direction.top;
         let newRightCoord = this.getCoords().right + this.direction.left;
 
-        console.log(this.measureFlightDelta());
         if(this.measureFlightDelta() >= this.flyRange) this.destroy()
 
         // bullet collision with screen boundaries
-        if(newTopCoord > 0 && newBottomCoord < window.innerHeight){
-            this.playerElem.style.top = newTopCoord + 'px';
+        if(newTopCoord > 0 && newBottomCoord < this.player.game.canvasElement.height){
+            this.position.top = newTopCoord;
         }
-        if(newLeftCoord > 0 && newRightCoord < window.innerWidth){
-            this.playerElem.style.left = newLeftCoord + 'px';
+        if(newLeftCoord > 0 && newRightCoord < this.player.game.canvasElement.width){
+            this.position.left = newLeftCoord;
         }
+
+        this.draw()
     }
 
     destroy(){
         let thisIndex = this.player.game.objects.findIndex(el=>el.id===this.id)
         this.player.game.objects.splice(thisIndex, 1)
-        this.playerElem.remove();
     }
 
     getCoords(){
-        return this.playerElem.getBoundingClientRect()
+        return {
+            top: this.position.top,
+            left: this.position.left,
+            bottom: this.position.top + this.height,
+            right: this.position.left + this.width
+        }
     }
 }
