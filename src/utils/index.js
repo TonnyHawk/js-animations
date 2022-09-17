74089c2dba832c5994da0842a78fe0f4be4cf4da
Anthonii Veremko <anton.veremko@gmail.com>
2022-09-17 13:02:11 +0200

    Utility function to get timestamps added

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,3 +2,11 @@ export function getModulus(num){
     if(num >= 0) return num
     else return num * -1
 }
+
+export function getTimestampInSeconds () {
+    return Math.floor(Date.now() / 1000)
+}
+
+export function getTimestamp () {
+    return Math.floor(Date.now())
+}
