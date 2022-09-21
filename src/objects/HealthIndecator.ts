333d8f48c741effeab2a85edb3e849115c0ac18d
Anthonii Veremko <anton.veremko@gmail.com>
2022-09-21 21:03:52 +0200

    Health Indicator updated with ability to show indicator value as text

--- src/objects/HealthIndecator.ts
+++ src/objects/HealthIndecator.ts
@@ -5,16 +5,29 @@ import Game from "./Game";
 export default class HealthIndicator extends Indicator {
 	element: HTMLElement;
 	progressLineElement: HTMLElement;
+	textElement: HTMLElement;
+	text: string;
 	constructor(game: Game, target: GameObject) {
 		super(game, target);
 		this.element = document.getElementById("player-health-indicator") as HTMLElement;
 		this.progressLineElement = this.element.querySelector(".indicator__progress-bar-main-line") as HTMLElement;
+		this.textElement = this.element.querySelector(".indicator__progress-bar-text") as HTMLElement;
+		this.text = `${this.value.current}`;
+	}
+
+	update(): void {
+		this.value = {
+			max: this.target.hp.full,
+			current: this.target.hp.available,
+		};
+		this.text = `${this.value.current}`;
 	}
 
 	draw() {
 		this.update();
 		if (this.progressLineElement) {
-			this.progressLineElement.style.width = `${this.value.current}%`;
+			this.progressLineElement.style.width = `${(this.value.current * 100) / this.value.max}%`;
+			this.textElement.textContent = this.text;
 		}
 	}
 }
