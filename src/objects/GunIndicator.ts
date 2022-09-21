df66c8060996b156a778d9a382bbe9af6edf281b
Anthonii Veremko <anton.veremko@gmail.com>
2022-09-21 21:01:22 +0200

    GunIndicator object created

---
+++ src/objects/GunIndicator.ts
@@ -0,0 +1,34 @@
+import Indicator from "./basic/indicator";
+import Person from "./basic/Person";
+import Game from "./Game";
+
+export default class GunIndicator extends Indicator {
+	element: HTMLElement;
+	progressLineElement: HTMLElement;
+	textElement: HTMLElement;
+	text: string;
+	constructor(game: Game, target: Person) {
+		super(game, target);
+		this.element = document.getElementById("player-gun-indicator") as HTMLElement;
+		this.progressLineElement = this.element.querySelector(".indicator__progress-bar-main-line") as HTMLElement;
+		this.textElement = this.element.querySelector(".indicator__progress-bar-text") as HTMLElement;
+		this.text = `${this.value.current}`;
+	}
+	update(): void {
+		if (this.target.gun) {
+			this.value = {
+				max: this.target.gun.clipSize,
+				current: this.target.gun.bulletsInClip,
+			};
+			if (!this.target.gun.isReloading) this.text = `${this.value.current}`;
+			else this.text = "Reloading...";
+		}
+	}
+	draw() {
+		this.update();
+		if (this.progressLineElement) {
+			this.progressLineElement.style.width = `${(this.value.current * 100) / this.value.max}%`;
+			this.textElement.textContent = this.text;
+		}
+	}
+}
