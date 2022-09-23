2aacafea3eb0d6ed24bd6571274a3a12113ec18d
Anthonii Veremko <anton.veremko@gmail.com>
2022-09-23 20:57:57 +0200

    Gunindicator feature. Reloading animation added

--- src/objects/GunIndicator.ts
+++ src/objects/GunIndicator.ts
@@ -10,6 +10,16 @@ export default class GunIndicator extends Indicator {
 		this.fillColor = "#3FB0F0";
 	}
 
+	updateValue(max: number, current: number): void {
+		if (this.target.gun) {
+			this.value = { max, current };
+			if (this.target.gun.isReloading) {
+				this.value.max = this.target.gun.reloadingTime.full;
+				this.value.current = this.target.gun.reloadingTime.full - this.target.gun.reloadingTime.left;
+			}
+		}
+	}
+
 	update(): void {
 		if (this.target.gun) {
 			this.updateValue(this.target.gun.clipSize, this.target.gun.bulletsInClip);
