985a35e2ec9f15a08601db6882f6b816ad827f0f
Anthonii Veremko <anton.veremko@gmail.com>
2022-09-23 20:46:40 +0200

    Health indicator restructured according to the changed indicator object

--- src/objects/HealthIndicator.ts
+++ src/objects/HealthIndicator.ts
@@ -1,54 +1,17 @@
-import { coordinate } from "../types";
 import GameObject from "./basic/GameObject";
 import Indicator from "./basic/indicator";
 import Person from "./basic/Person";
 import Game from "./Game";
 
 export default class HealthIndicator extends Indicator {
-	height: number;
-	width: number;
-	position: coordinate;
 	constructor(game: Game, target: Person | GameObject) {
 		super(game, target);
-		this.height = 10;
-		this.width = 80;
-		this.position = {
-			top: this.target.position.top + 20,
-			left: (this.width - this.target.width) / 2,
-		};
-
-		this.game.indicators.push(this);
-		this.update();
-	}
-
-	updateValue() {
-		this.value = {
-			max: this.target.hp.full,
-			current: this.target.hp.available,
-		};
-	}
-
-	updatePosition() {
-		const left = (this.width - this.target.width) / 2;
-		this.position = {
-			top: this.target.position.top - (10 + this.height),
-			left: this.target.position.left - left,
-		};
+		this.marginToOwner = 20;
+		this.fillColor = "#EA6F6F";
 	}
 
 	update(): void {
-		this.updateValue();
+		this.updateValue(this.target.hp.full, this.target.hp.available);
 		this.updatePosition();
-	}
-
-	draw(): void {
-		this.update();
-		if (this.game.screen) {
-			const { screen } = this.game;
-			screen.fillStyle = "#D9D9D9";
-			screen.fillRect(this.position.left, this.position.top, this.width, this.height);
-			screen.fillStyle = "#EA6F6F";
-			screen.fillRect(this.position.left, this.position.top, (this.value.current * this.width) / this.value.max, this.height);
-		}
 	}
 }
