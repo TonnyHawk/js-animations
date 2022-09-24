3a4ffc0b2c164193f0f52c72e8771e9b5a98072e
Anthonii Veremko <anton.veremko@gmail.com>
2022-09-24 17:30:21 +0200

    Player gun indicator updated with new text for gun reloading

--- src/objects/indicators/PlayerGunIndicator.ts
+++ src/objects/indicators/PlayerGunIndicator.ts
@@ -1,9 +1,9 @@
 import GameObject from "../basic/GameObject";
 import Person from "../basic/Person";
 import Game from "../Game";
-import GunIndicator from "./GunIndicator";
+import Indicator from "./Indicator";
 
-export default class PlayerGunIndicator extends GunIndicator {
+export default class PlayerGunIndicator extends Indicator {
 	pageElement: {
 		available: HTMLElement;
 		full: HTMLElement;
@@ -21,10 +21,10 @@ export default class PlayerGunIndicator extends GunIndicator {
 		if (this.target.gun) {
 			// update indicator element on the page
 			if (!this.target.gun.isReloading) {
-				this.pageElement.available.innerText = "" + this.value.current;
-				this.pageElement.full.innerText = "" + this.value.max;
+				this.pageElement.available.innerText = "" + this.target.gun.bulletsInClip;
+				this.pageElement.full.innerText = "" + this.target.gun.clipSize;
 			} else {
-				this.pageElement.available.innerText = "0";
+				this.pageElement.available.innerText = "Reloading....";
 			}
 		}
 	}
