7d960c70b88baf355b95358deb4db04342191338
Anthonii Veremko <anton.veremko@gmail.com>
2022-12-23 12:32:20 +0100

    Bullet item has now descrition fo inventory

--- src/objects/inventory/items/BulletItem.ts
+++ src/objects/inventory/items/BulletItem.ts
@@ -4,8 +4,12 @@ import InventoryItem from "./InventoryItem";
 export default class BulletItem extends InventoryItem {
 	constructor(game: Game, left: number, top: number) {
 		super(game, left, top);
-		this.itemName = "Bullet";
-		this.amount = 23;
 		this.image = document.getElementById("inventory-item-bullets") as HTMLImageElement;
+		this.description = {
+			name: "Bullet",
+			amount: 23,
+			image: this.image.src,
+			text: "No gun can shot without bullet, right? Applicable to any weapon",
+		};
 	}
 }
